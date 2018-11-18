3130b8cd11b46e2d8048e31c0ecedfa7a3dec1ea
bjh970913 <jihun.baek@tgnine.com>
2018-11-18 21:00:30 +0900

    Update

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,12 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
+/*
 Java_works_bjh_study_jetpack_MainActivity_stringFromJNI(
         JNIEnv* env,
-        jobject /* this */) {
+        jobject) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
+*/
